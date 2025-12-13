849094d90a5f60c98358ab725d98c8dbff3db8dc
kearmand <kearmand@student.42.fr>
2025-12-13 19:05:03 +0100

    mesage

--- src/engine/init_surface_uv.c
+++ src/engine/init_surface_uv.c
@@ -6,7 +6,7 @@
 /*   By: kearmand <kearmand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/01 16:31:16 by kearmand          #+#    #+#             */
-/*   Updated: 2025/12/04 17:02:14 by kearmand         ###   ########.fr       */
+/*   Updated: 2025/12/13 18:19:07 by kearmand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,248 +14,15 @@
 #include "vector.h"
 #include <math.h>
 
-static t_texture	*get_albedo_tex(t_scene *sc, t_primitive *p)
+static void	init_surface_uv_one(t_surface *surface, const t_primitive *primitive)
 {
-	t_material	*m;
-	int			tid;
+	float	det;
 
-	m = &sc->materials[p->material_id];
-	tid = m->texture_albedo_id;
-	if (tid < 0)
-		return (0);
-	return (&sc->textures[tid]);
-}
-
-static void	init_uv_mode0(t_surface *s, t_plane *pl)
-{
-	t_vec3f	u;
-	t_vec3f	v;
-
-	if (fabsf(pl->normal[0]) < 0.9f)
-		u = (t_vec3f){1.0f, 0.0f, 0.0f};
-	else
-		u = (t_vec3f){0.0f, 1.0f, 0.0f};
-	u = proj_on_plane(u, pl->normal);
-	u = vec3f_normalize(u);
-	v = vec3f_cross(pl->normal, u);
-	v = vec3f_normalize(v);
-	s->map_uv[0] = u[0];
-	s->map_uv[1] = u[1];
-	s->map_uv[2] = u[2];
-	s->map_uv[3] = v[0];
-	s->map_uv[4] = v[1];
-	s->map_uv[5] = v[2];
-	s->scale_u = 1.0f;
-	s->scale_v = 0.0f;
-}
-
-static void	init_uv_mode1(t_surface *s, t_plane *pl)
-{
-	t_vec3f	u;
-	t_vec3f	v;
-	float	len;
-
-	u = (t_vec3f){s->map_uv[0], s->map_uv[1], s->map_uv[2]};
-	u = proj_on_plane(u, pl->normal);
-	len = vec3f_length(u);
-	if (len < 1e-6f)
-		return (init_uv_mode0(s, pl));
-	u = vec3f_scale(u, 1.0f / len);
-	v = vec3f_cross(pl->normal, u);
-	v = vec3f_normalize(v);
-	s->map_uv[0] = u[0];
-	s->map_uv[1] = u[1];
-	s->map_uv[2] = u[2];
-	s->map_uv[3] = v[0];
-	s->map_uv[4] = v[1];
-	s->map_uv[5] = v[2];
-	s->scale_u = len;
-	s->scale_v = 0.0f;
-}
-
-static void	init_uv_mode2(t_surface *s, t_plane *pl)
-{
-	t_vec3f	u;
-	t_vec3f	v;
-	float	lu;
-	float	lv;
-
-	u = (t_vec3f){s->map_uv[0], s->map_uv[1], s->map_uv[2]};
-	v = (t_vec3f){s->map_uv[3], s->map_uv[4], s->map_uv[5]};
-	u = proj_on_plane(u, pl->normal);
-	v = proj_on_plane(v, pl->normal);
-	lu = vec3f_length(u);
-	if (lu < 1e-6f)
-		return (init_uv_mode0(s, pl));
-	lv = vec3f_length(v);
-	if (lv < 1e-6f)
-		return (init_uv_mode1(s, pl));
-	u = vec3f_scale(u, 1.0f / lu);
-	v = vec3f_scale(v, 1.0f / lv);
-	s->map_uv[0] = u[0];
-	s->map_uv[1] = u[1];
-	s->map_uv[2] = u[2];
-	s->map_uv[3] = v[0];
-	s->map_uv[4] = v[1];
-	s->map_uv[5] = v[2];
-	s->scale_u = lu;
-	s->scale_v = lv;
-}
-
-static void	apply_texture_ratio(t_surface *s, t_texture *t)
-{
-	float	r;
-
-	if (!t || t->width == 0 || t->height == 0)
-	{
-		s->scale_v = s->scale_u;
+	if (primitive->type == PRIM_TRIANGLE)
 		return ;
-	}
-	r = (float)t->height / (float)t->width;
-	s->scale_v = s->scale_u * r;
-}
-
-static void	init_surface_uv_plane(t_scene *sc, t_surface *s, t_primitive *p)
-{
-	t_plane		*pl;
-	t_texture	*tex;
-
-	pl = &p->pl;
-	s->normal = pl->normal;
-	if (s->uv_mod == 0)
-		init_uv_mode0(s, pl);
-	else if (s->uv_mod == 1)
-		init_uv_mode1(s, pl);
-	else
-		init_uv_mode2(s, pl);
-	if (s->scale_v == 0.0f)
-	{
-		tex = get_albedo_tex(sc, p);
-		apply_texture_ratio(s, tex);
-	}
-}
-
-static void	sphere_dir_to_uv(t_vec3f direction,
-				float *u_out, float *v_out)
-{
-	t_vec3f	normalized_dir;
-	float	theta_angle;
-	float	phi_angle;
-	float	u_value;
-
-	normalized_dir = vec3f_normalize(direction);
-	theta_angle = atan2f(normalized_dir[2], normalized_dir[0]);
-	phi_angle = acosf(normalized_dir[1]);
-	u_value = 0.5f + theta_angle / (2.0f * (float)M_PI);
-	if (u_value < 0.0f)
-		u_value += 1.0f;
-	else if (u_value >= 1.0f)
-		u_value -= 1.0f;
-	*u_out = u_value;
-	*v_out = phi_angle / (float)M_PI;
-}
-
-
-static void	init_surface_uv_sphere_mode1(t_surface *surface)
-{
-	t_vec3f	anchor_vector;
-	float	anchor_u;
-	float	anchor_v;
-
-	anchor_vector = (t_vec3f){surface->map_uv[0],
-		surface->map_uv[1], surface->map_uv[2]};
-	sphere_dir_to_uv(anchor_vector, &anchor_u, &anchor_v);
-	surface->map_uv[0] = 1.0f;
-	surface->map_uv[1] = 0.0f;
-	surface->map_uv[2] = 0.0f;
-	surface->map_uv[3] = 1.0f;
-	surface->map_uv[4] = 0.5f - anchor_u;
-	surface->map_uv[5] = 0.5f - anchor_v;
-}
-
-static float	wrap_u_relative(float value, float reference)
-{
-	float	diff;
-
-	diff = value - reference;
-	if (diff > 0.5f)
-		value -= 1.0f;
-	else if (diff < -0.5f)
-		value += 1.0f;
-	return (value);
-}
-
-
-
-static void	init_surface_uv_sphere_mode2(t_surface *surface)
-{
-	t_vec3f	center_vector;
-	t_vec3f	up_vector;
-	float	center_u;
-	float	center_v;
-	float	up_u;
-	float	up_v;
-	float	adjusted_up_u;
-	float	delta_u;
-	float	delta_v;
-	float	source_length;
-	float	unit_src_u;
-	float	unit_src_v;
-	float	cosine_angle;
-	float	sine_angle;
-	float	scale_factor;
-	float	coef_uu;
-	float	coef_uv;
-	float	coef_vu;
-	float	coef_vv;
-	float	offset_u;
-	float	offset_v;
-
-	center_vector = (t_vec3f){surface->map_uv[0],
-		surface->map_uv[1], surface->map_uv[2]};
-	up_vector = (t_vec3f){surface->map_uv[3],
-		surface->map_uv[4], surface->map_uv[5]};
-	sphere_dir_to_uv(center_vector, &center_u, &center_v);
-	sphere_dir_to_uv(up_vector, &up_u, &up_v);
-	adjusted_up_u = wrap_u_relative(up_u, center_u);
-	delta_u = adjusted_up_u - center_u;
-	delta_v = up_v - center_v;
-	source_length = sqrtf(delta_u * delta_u + delta_v * delta_v);
-	if (source_length < 1e-6f)
-	{
-		init_surface_uv_sphere_mode1(surface);
-		return ;
-	}
-	unit_src_u = delta_u / source_length;
-	unit_src_v = delta_v / source_length;
-	cosine_angle = unit_src_v;
-	sine_angle = unit_src_u;
-	scale_factor = 0.5f / source_length;
-	coef_uu = scale_factor * cosine_angle;
-	coef_uv = -scale_factor * sine_angle;
-	coef_vu = scale_factor * sine_angle;
-	coef_vv = scale_factor * cosine_angle;
-	offset_u = 0.5f
-		- (coef_uu * center_u + coef_uv * center_v);
-	offset_v = 0.5f
-		- (coef_vu * center_u + coef_vv * center_v);
-	surface->map_uv[0] = coef_uu;
-	surface->map_uv[1] = coef_uv;
-	surface->map_uv[2] = coef_vu;
-	surface->map_uv[3] = coef_vv;
-	surface->map_uv[4] = offset_u;
-	surface->map_uv[5] = offset_v;
-}
-
-
-
-void	init_surface_uv_sphere(t_scene *scene, t_surface *surface,
-				t_primitive *primitive)
-{
-	(void)scene;
-	(void)primitive;
-	surface->normal = (t_vec3f){0.0f, 0.0f, 0.0f};
-	if (surface->uv_mod == 0)
+	det = surface->map_uv[0] * surface->map_uv[3]
+		- surface->map_uv[1] * surface->map_uv[2];
+	if (fabsf(det) < 1e-6f)
 	{
 		surface->map_uv[0] = 1.0f;
 		surface->map_uv[1] = 0.0f;
@@ -264,37 +31,26 @@ void	init_surface_uv_sphere(t_scene *scene, t_surface *surface,
 		surface->map_uv[4] = 0.0f;
 		surface->map_uv[5] = 0.0f;
 	}
-	else if (surface->uv_mod == 1)
-		init_surface_uv_sphere_mode1(surface);
-	else
-		init_surface_uv_sphere_mode2(surface);
 }
 
-
-
-void	init_surface_uv(t_scene *sc)
+void	init_surface_uv(t_scene *scene)
 {
-	int			i;
-	t_surface	*s;
-	t_primitive	*p;
+	int			index;
+	t_primitive	*primitive;
+	t_surface	*surface;
 
-	i = 0;
-	while (i < sc->plane_count)
+	index = 0;
+	while (index < scene->plane_count)
 	{
-		p = &sc->planes[i];
-		s = &sc->surfaces[p->surface_id];
-		init_surface_uv_plane(sc, s, p);
-		i++;
+		primitive = &scene->planes[index++];
+		surface = &scene->surfaces[primitive->surface_id];
+		init_surface_uv_one(surface, primitive);
 	}
-	i = 0;
-	while (i < sc->primitive_count)
+	index = 0;
+	while (index < scene->primitive_count)
 	{
-		p = &sc->primitives[i];
-		s = &sc->surfaces[p->surface_id];
-		if (p->type == PRIM_SPHERE)
-			init_surface_uv_sphere(sc, s, p);
-		// if (p->type == PRIM_CYLINDER
-		// 	init_surface_uv_cylinder(sc, s, p);
-		i++;
+		primitive = &scene->primitives[index++];
+		surface = &scene->surfaces[primitive->surface_id];
+		init_surface_uv_one(surface, primitive);
 	}
 }
--- src/engine/shading/hit_build_geometry.c
+++ src/engine/shading/hit_build_geometry.c
@@ -5,154 +5,201 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: kearmand <kearmand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/11/22 17:24:31 by kearmand          #+#    #+#             */
-/*   Updated: 2025/12/12 18:41:46 by kearmand         ###   ########.fr       */
+/*   Created: 2025/12/13 17:32:19 by kearmand          #+#    #+#             */
+/*   Updated: 2025/12/13 19:01:45 by kearmand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "engine.h"
 #include "scene.h"
-#include <stdio.h>
 #include <math.h>
 
-static void	build_geometry_plane(const t_scene *scene, t_hit *hit)
+static void	hit_set_point_and_view(const t_ray *ray, t_hit *hit)
 {
-	t_primitive	*prim;
-	t_surface	*surf;
-	t_vec3f		origin;
-	t_vec3f		delta;
+	t_vec3f	point;
 
-	prim = &scene->planes[hit->primitive_id];
-	surf = &scene->surfaces[prim->surface_id];
-	hit->surface_id = prim->surface_id;
-	hit->material_id = prim->material_id;
-	hit->normal = prim->pl.normal;
-	origin = vec3f_scale(prim->pl.normal, -prim->pl.d);
-	delta = vec3f_sub(hit->point, origin);
-	hit->u = vec3f_dot(delta, (t_vec3f){surf->map_uv[0],
-			surf->map_uv[1], surf->map_uv[2]}) / surf->scale_u;
-	hit->v = vec3f_dot(delta, (t_vec3f){surf->map_uv[3],
-			surf->map_uv[4], surf->map_uv[5]}) / surf->scale_v;
+	point = vec3f_add(ray->origin, vec3f_scale(ray->dir, hit->t));
+	hit->point = point;
+	hit->view_dir = vec3f_scale(ray->dir, -1.0f);
 }
-void	surface_apply_uv_sphere(const t_surface *surface,
-				float base_u, float base_v,
-				float *mapped_u, float *mapped_v)
-{
-	float	raw_u;
-	float	raw_v;
-	float	wrapped_u;
-	float	wrapped_v;
 
-	raw_u = surface->map_uv[0] * base_u
+static void	hit_faceforward(const t_ray *ray, t_hit *hit)
+{
+	if (vec3f_dot(hit->normal, ray->dir) > 0.0f)
+		hit->normal = vec3f_scale(hit->normal, -1.0f);
+}
+
+void	surface_apply_uv(const t_surface *surface, t_hit *hit)
+{
+	float	base_u;
+	float	base_v;
+
+	base_u = hit->u;
+	base_v = hit->v;
+	hit->u = surface->map_uv[0] * base_u
 		+ surface->map_uv[1] * base_v
 		+ surface->map_uv[4];
-	raw_v = surface->map_uv[2] * base_u
+	hit->v = surface->map_uv[2] * base_u
 		+ surface->map_uv[3] * base_v
 		+ surface->map_uv[5];
-	wrapped_u = raw_u - floorf(raw_u);
-	wrapped_v = raw_v - floorf(raw_v);
-	*mapped_u = wrapped_u;
-	*mapped_v = wrapped_v;
 }
 
 
-static void build_geometry_sphere(const t_sphere *sphere,
-			t_hit *hit)
+
+void	build_geometry_plane(const t_scene *scene, t_hit *hit)
 {
-	t_vec3f	p_local;
-	float	theta;
+	const t_primitive	*plane;
+	t_vec3f				normal;
+	t_vec3f				axis;
+	t_vec3f				tangent;
+	t_vec3f				bitangent;
+	t_vec3f				origin;
+	t_vec3f				delta;
+
+	plane = &scene->planes[hit->primitive_id];
+	normal = plane->pl.normal;
+	hit->normal = normal;
+	axis = (t_vec3f){1.0f, 0.0f, 0.0f};
+	if (fabsf(normal[0]) > 0.95f)
+		axis = (t_vec3f){0.0f, 1.0f, 0.0f};
+	tangent = vec3f_sub(axis, vec3f_scale(normal, vec3f_dot(axis, normal)));
+	tangent = vec3f_normalize(tangent);
+	bitangent = vec3f_normalize(vec3f_cross(normal, tangent));
+	origin = vec3f_scale(normal, -plane->pl.d);
+	delta = vec3f_sub(hit->point, origin);
+	hit->u = vec3f_dot(delta, tangent);
+	hit->v = vec3f_dot(delta, bitangent);
+}
+
+static void	hit_build_plane(const t_scene *scene, t_hit *hit)
+{
+	const t_primitive	*plane_prim;
+
+	plane_prim = &scene->planes[hit->primitive_id];
+	hit->surface_id = plane_prim->surface_id;
+	hit->material_id = plane_prim->material_id;
+	build_geometry_plane(scene, hit);
+}
+
+static float	clamp_unit(float value)
+{
+	if (value > 1.0f)
+		return (1.0f);
+	if (value < -1.0f)
+		return (-1.0f);
+	return (value);
+}
+
+static void	build_primitive_sphere(const t_sphere *sphere,
+				t_hit *hit, t_vec3f *normal)
+{
+	t_vec3f	local;
 	float	phi;
+	float	theta;
 
-	p_local = vec3f_sub(hit->point, sphere->center);
-	theta = acosf(fmaxf(fminf(p_local[1] / sphere->radius, 1.0f), -1.0f));
-	phi = atan2f(p_local[2], p_local[0]);
+	local = vec3f_sub(hit->point, sphere->center);
+	*normal = vec3f_normalize(local);
+	phi = atan2f(local[2], local[0]);
 	if (phi < 0.0f)
-		phi += 2.0f * M_PI;
-	hit->u = 0.7 - phi / (2.0f * M_PI);
-	hit->v = - (theta / M_PI);
+		phi += 2.0f * (float)M_PI;
+	theta = acosf(clamp_unit(local[1] / sphere->radius));
+	hit->u = phi / (2.0f * (float)M_PI);
+	hit->v = theta / (float)M_PI;
 }
-
-static void	build_normal_sphere(const t_sphere *sphere,
-			const t_hit *hit, t_vec3f *normal)
+static t_vec3f	cyl_ref_axis(t_vec3f axis)
 {
-	t_vec3f	diff;
-
-	diff = vec3f_sub(hit->point, sphere->center);
-	*normal = vec3f_normalize(diff);
-	build_geometry_sphere(sphere, hit);
+	if (fabsf(axis[0]) > 0.95f)
+		return ((t_vec3f){0.0f, 1.0f, 0.0f});
+	return ((t_vec3f){1.0f, 0.0f, 0.0f});
 }
 
-static void	build_normal_cylinder(const t_cylinder *cylinder,
-			const t_hit *hit, t_vec3f *normal)
+static void	cyl_build_frame(t_vec3f axis, t_vec3f *tangent, t_vec3f *bitangent)
+{
+	t_vec3f	ref;
+
+	ref = cyl_ref_axis(axis);
+	*tangent = vec3f_normalize(vec3f_cross(axis, ref));
+	*bitangent = vec3f_cross(axis, *tangent);
+}
+
+static float	cyl_angle_u(t_vec3f radial_unit, t_vec3f tangent, t_vec3f bitangent)
+{
+	float	x;
+	float	y;
+	float	a;
+
+	x = vec3f_dot(radial_unit, tangent);
+	y = vec3f_dot(radial_unit, bitangent);
+	a = atan2f(y, x);
+	if (a < 0.0f)
+		a += 2.0f * (float)M_PI;
+	return (a / (2.0f * (float)M_PI));
+}
+
+static void	build_primitive_cylinder(const t_cylinder *cylinder,
+				t_hit *hit, t_vec3f *normal)
 {
 	t_vec3f	base_to_point;
 	t_vec3f	proj_point;
 	t_vec3f	radial;
+	t_vec3f	tangent;
+	t_vec3f	bitangent;
 	float	height_pos;
 
 	base_to_point = vec3f_sub(hit->point, cylinder->base);
 	height_pos = vec3f_dot(base_to_point, cylinder->axis);
-	proj_point = vec3f_add(cylinder->base,
-			vec3f_scale(cylinder->axis, height_pos));
+	proj_point = vec3f_add(cylinder->base, vec3f_scale(cylinder->axis, height_pos));
 	radial = vec3f_sub(hit->point, proj_point);
 	*normal = vec3f_normalize(radial);
+	cyl_build_frame(cylinder->axis, &tangent, &bitangent);
+	hit->u = cyl_angle_u(*normal, tangent, bitangent);
+	hit->v = height_pos / cylinder->height;
 }
 
-static void	build_normal_triangle(const t_triangle *triangle,
-			t_vec3f *normal)
+static void	build_primitive_triangle(const t_primitive *primitive,
+				t_hit *hit, const t_surface *surface)
 {
-	t_vec3f	cross;
+	const t_triangle	*tria;
 
-	cross = vec3f_cross(triangle->edge1, triangle->edge2);
-	*normal = vec3f_normalize(cross);
+	tria = &primitive->tr;
+
+	hit->normal = surface->normal;
+	// coordonnées barycentriques ( on el fait apres  :)
+	hit->u = 0.0f;
+	hit->v = 0.0f;
 }
 
-static void	build_normal_primitive(const t_primitive *primitive,
-			const t_hit *hit, t_vec3f *normal)
-{
-	if (primitive->type == PRIM_SPHERE)
-		build_normal_sphere(&primitive->sp, hit, normal);
-	else if (primitive->type == PRIM_CYLINDER)
-		build_normal_cylinder(&primitive->cy, hit, normal);
-	else if (primitive->type == PRIM_TRIANGLE)
-		build_normal_triangle(&primitive->tr, normal);
-}
-#include <stdio.h>
-void	hit_build_geometry(const t_scene *scene,
-			const t_ray *ray, t_hit *hit)
+
+static void	hit_build_primitive(const t_scene *scene, t_hit *hit)
 {
 	const t_primitive	*primitive;
-	t_vec3f				point;
-	t_vec3f				normal;
+	const t_surface		*surface;
 
-	normal = (t_vec3f){0.0f, 0.0f, 0.0f};
-	point = vec3f_add(ray->origin, vec3f_scale(ray->dir, hit->t));
-	hit->point = point;
+	primitive = &scene->primitives[hit->primitive_id];
+	hit->surface_id = primitive->surface_id;
+	hit->material_id = primitive->material_id;
+	surface = &scene->surfaces[hit->surface_id];
+	if (primitive->type == PRIM_SPHERE)
+		build_primitive_sphere(&primitive->sp, hit, &hit->normal);
+	else if (primitive->type == PRIM_CYLINDER)
+		build_primitive_cylinder(&primitive->cy, hit, &hit->normal);
+	else if (primitive->type == PRIM_TRIANGLE)
+		build_primitive_triangle(primitive, hit, surface);
+	if (primitive->type != PRIM_TRIANGLE)
+		surface_apply_uv(surface, hit);
+}
+
+
+void	hit_build_geometry(const t_scene *scene, const t_ray *ray, t_hit *hit)
+{
+	hit->normal = (t_vec3f){0.0f, 0.0f, 0.0f};
+	hit_set_point_and_view(ray, hit);
 	if (hit->kind == HIT_PLANE)
-	{
-		hit->surface_id = scene->planes[hit->primitive_id].surface_id;
-		hit->material_id = scene->planes[hit->primitive_id].material_id;
-		build_geometry_plane(scene, hit);
-		normal = scene->planes[hit->primitive_id].pl.normal;
-	}
+		hit_build_plane(scene, hit);
 	else if (hit->kind == HIT_PRIMITIVE)
-	{
-		hit->surface_id = scene->primitives[hit->primitive_id].surface_id;
-		hit->material_id = scene->primitives[hit->primitive_id].material_id;
-		primitive = &scene->primitives[hit->primitive_id];
-		hit->albedo = scene->surfaces[hit->surface_id].color;
-		build_normal_primitive(primitive, hit, &normal);
-		if (primitive->type == PRIM_SPHERE)
-			surface_apply_uv_sphere(
-				&scene->surfaces[hit->surface_id],
-				hit->u, hit->v,
-				&hit->u, &hit->v);
-	}
-	else
-		printf("ca pue du cul dans hit_build_geometry\n");
-
-	hit->view_dir = vec3f_scale(ray->dir, -1.0f);
-	hit->normal = normal;
-	if (vec3f_dot(hit->normal, ray->dir) > 0.0f)
-	 	hit->normal = vec3f_scale(hit->normal, -1.0f);
+		hit_build_primitive(scene, hit);
+	
+	hit_faceforward(ray, hit);
+	if (hit->normal[0] == 0.0f && hit->normal[1] == 0.0f && hit->normal[2] == 0.0f)
+		printf("normal pas build dans hit_build_geometry\n");
 }
